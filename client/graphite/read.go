0f7c5e0552f23f1a0320d278bc87163bf4259c64
Ruben Vanoverschelde <ruben.vanoverschelde@kuleuven.be>
2019-01-23 14:06:33 +0100

    Use correct data type

--- client/graphite/read.go
+++ client/graphite/read.go
@@ -77,7 +77,7 @@ func (c *Client) queryToTargets(ctx context.Context, query *prompb.Query, graphi
 		return nil, err
 	}
 
-	mock_data := [2]string{"{\"allowChildren\":1,\"expandable\":1,\"leaf\":0,\"id\":\"development.applications.prometheus.container_cpu_load_average_10s.container_label_build_date\",\"text\":\"container_label_build_date\",\"context\":{}}", "{\"allowChildren\":1,\"expandable\":1,\"leaf\":0,\"id\":\"development.applications.prometheus.container_cpu_load_average_10s.container_label_maintainer\",\"text\":\"container_label_maintainer\",\"context\":{}}"}
+	mock_data := []string{"{\"allowChildren\":1,\"expandable\":1,\"leaf\":0,\"id\":\"development.applications.prometheus.container_cpu_load_average_10s.container_label_build_date\",\"text\":\"container_label_build_date\",\"context\":{}}", "{\"allowChildren\":1,\"expandable\":1,\"leaf\":0,\"id\":\"development.applications.prometheus.container_cpu_load_average_10s.container_label_maintainer\",\"text\":\"container_label_maintainer\",\"context\":{}}"}
 	targets, err := c.filterTargets(query, mock_data, graphitePrefix)
 	return targets, err
 }
