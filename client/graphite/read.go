bc552b799acb67b546a0d739534097533b3d48bd
Ruben Vanoverschelde <ruben.vanoverschelde@kuleuven.be>
2019-01-23 10:33:40 +0100

    Recursive search

--- client/graphite/read.go
+++ client/graphite/read.go
@@ -49,10 +49,9 @@ func (c *Client) queryToTargets(ctx context.Context, query *prompb.Query, graphi
 	}
 
 	// Prepare the url to fetch
-	queryStr := graphitePrefix + name + ".**"
+	queryStr := graphitePrefix + name + ".*.**"
 	expandURL, err := prepareURL(c.cfg.Read.URL, expandEndpoint, map[string]string{"format": "json", "leavesOnly": "1", "query": queryStr})
 	level.Info(c.logger).Log("graphite_web", c.cfg.Read.URL, "path", expandEndpoint)
-	//level.Info(c.logger).Log("message", "Logging test")
 	if err != nil {
 		level.Warn(c.logger).Log(
 			"graphite_web", c.cfg.Read.URL, "path", expandEndpoint,
@@ -60,10 +59,8 @@ func (c *Client) queryToTargets(ctx context.Context, query *prompb.Query, graphi
 		return nil, err
 	}
 
-	//level.Info(c.logger).Log("message", "Logging test2")
 	// Get the list of targets
 	expandResponse := ExpandResponse{}
-	//level.Info(c.logger).Log("message", "Logging test3")
 	body, err := fetchURL(ctx, c.logger, expandURL)
 	level.Info(c.logger).Log("body", body)
 	if err != nil {
